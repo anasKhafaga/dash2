d7aa247d1b8f6c7e29dfffdf3f27a99376118513
Anas Khafaga <khafaga1998@gmail.com>
2020-06-16 01:02:50 +0200

    fetch posts from database

--- includes/functions.php
+++ includes/functions.php
@@ -4,10 +4,21 @@
 function getCat() {
   global $connection;
   $query = "SELECT * FROM `categories`";
-  $arr = [];
   $cursor = mysqli_query($connection, $query);
+  $arr = [];
   while($row = mysqli_fetch_assoc($cursor)){
     array_push($arr, $row['cat_title']);
+  };
+  return $arr;
+};
+
+function getPosts() {
+  global $connection;
+  $query = "SELECT * FROM `posts`";
+  $cursor = mysqli_query($connection, $query);
+  $arr = [];
+  while($row = mysqli_fetch_assoc($cursor)){
+    array_push($arr, $row);
   };
   return $arr;
 };
--- index.php
+++ index.php
@@ -11,28 +11,40 @@
         <div class="col-md-8">
 
             <h1 class="page-header">
-                Page Heading
-                <small>Secondary Text</small>
+                Timeline
             </h1>
 
             <!-- First Blog Post -->
-            <h2>
-                <a href="#">Blog Post Title</a>
-            </h2>
-            <p class="lead">
-                by <a href="index.php">Start Bootstrap</a>
-            </p>
-            <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
-            <hr>
-            <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-            <hr>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
-            <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+            <?php
 
-            <hr>
+                $posts = getPosts();
+                foreach($posts as $post){
+                    foreach($post as $key => $value){
+                        $$key = $value;
+                    };
+                    echo "
+                        <h2>
+                            <a href='#'>{$title}</a>
+                        </h2>
+                        <p class='lead'>
+                            by <a href='index.php'>{$author}</a>
+                        </p>
+                        <p><span class='glyphicon glyphicon-time'></span> Posted on {$created_at}</p>
+                        <p><span class='glyphicon glyphicon-time'></span> Last update {$modified_at}</p>
+                        <hr>
+                        <img class='img-responsive' src={$image} alt=''>
+                        <hr>
+                        <p>{$content}</p>
+                        <a class='btn btn-primary' href='#'>Read More <span class='glyphicon glyphicon-chevron-right'></span></a>
+
+                        <hr>
+                    ";
+                };
+                
+            ?>
 
             <!-- Second Blog Post -->
-            <h2>
+            <!-- <h2>
                 <a href="#">Blog Post Title</a>
             </h2>
             <p class="lead">
@@ -43,7 +55,7 @@
             <img class="img-responsive" src="http://placehold.it/900x300" alt="">
             <hr>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam, quasi, fugiat, asperiores harum voluptatum tenetur a possimus nesciunt quod accusamus saepe tempora ipsam distinctio minima dolorum perferendis labore impedit voluptates!</p>
-            <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+            <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a> -->
 
         </div>
 
